0e8c79cce42ea25213a2dc0677d4e59e7db85f6c
Saratii <friedchicken14821@gmail.com>
2025-09-20 16:39:43 -0500

    majorly optimized chunk loading looping

--- src/terrain/chunk_thread.rs
+++ src/terrain/chunk_thread.rs
@@ -48,7 +48,7 @@ pub struct MyMapGenTasks {
     pub generation_tasks: Vec<
         Task<(
             Vec<((i16, i16, i16), TerrainChunk, Mesh, Transform, Collider)>,
-            Vec<((i16, i16, i16), TerrainChunk, Mesh, Transform)>,
+            Vec<((i16, i16, i16), TerrainChunk, Transform)>,
         )>,
     >,
     pub chunks_being_generated: HashSet<(i16, i16, i16)>,
@@ -59,7 +59,7 @@ pub struct LoadChunkTasks {
     pub loading_tasks: Vec<
         Task<(
             Vec<((i16, i16, i16), TerrainChunk, Mesh, Transform, Collider)>,
-            Vec<((i16, i16, i16), TerrainChunk, Mesh, Transform)>,
+            Vec<((i16, i16, i16), TerrainChunk, Transform)>,
         )>,
     >,
     pub chunks_being_loaded: HashSet<(i16, i16, i16)>,
@@ -69,9 +69,7 @@ pub struct LoadChunkTasks {
 pub struct StagedChunksLoaded(pub HashMap<(i16, i16, i16), (Entity, TerrainChunk)>);
 
 struct ChunkSpawnCommand<B> {
-    pub bundles: Vec<B>,
-    pub chunk_datas: Vec<TerrainChunk>,
-    pub chunk_coords: Vec<(i16, i16, i16)>,
+    pub bundles: Vec<((i16, i16, i16), TerrainChunk, B)>,
 }
 
 impl<B> Command for ChunkSpawnCommand<B>
@@ -80,17 +78,21 @@ where
     <B as DynamicBundle>::Effect: NoBundleEffect,
 {
     fn apply(self, world: &mut World) {
-        let entities: Vec<Entity> = world.spawn_batch(self.bundles).collect();
+        let mut coords_chunks = Vec::with_capacity(self.bundles.len());
+        let bundles: Vec<B> = self
+            .bundles
+            .into_iter()
+            .map(|(coord, chunk_data, bundle)| {
+                coords_chunks.push((coord, chunk_data));
+                bundle
+            })
+            .collect();
+        let entities: Vec<Entity> = world.spawn_batch(bundles).collect();
         world.resource_scope(|world, mut chunk_map: Mut<ChunkMap>| {
             world.resource_scope(|world, mut chunk_data_file: Mut<ChunkDataFile>| {
                 world.resource_scope(|world, chunk_index_map: Mut<ChunkIndexMap>| {
                     let mut index_file = world.get_resource_mut::<ChunkIndexFile>().unwrap();
-                    for ((coord, chunk_data), entity) in self
-                        .chunk_coords
-                        .into_iter()
-                        .zip(self.chunk_datas.into_iter())
-                        .zip(entities.into_iter())
-                    {
+                    for ((coord, chunk_data), entity) in coords_chunks.into_iter().zip(entities) {
                         let mut locked_index_map = chunk_index_map.0.lock().unwrap();
                         create_chunk_file_data(
                             &chunk_data,
@@ -100,6 +102,7 @@ where
                             &mut index_file.0,
                         );
                         drop(locked_index_map);
+
                         chunk_map.0.insert(coord, (entity, chunk_data));
                     }
                 });
@@ -109,9 +112,7 @@ where
 }
 
 struct ChunkLoadCommand<B> {
-    pub bundles: Vec<B>,
-    pub chunk_datas: Vec<TerrainChunk>,
-    pub chunk_coords: Vec<(i16, i16, i16)>,
+    pub bundles: Vec<((i16, i16, i16), TerrainChunk, B)>,
 }
 
 impl<B> Command for ChunkLoadCommand<B>
@@ -120,15 +121,22 @@ where
     <B as DynamicBundle>::Effect: NoBundleEffect,
 {
     fn apply(self, world: &mut World) {
-        let entities: Vec<Entity> = world.spawn_batch(self.bundles).collect();
-        let mut staged = world.get_resource_mut::<StagedChunksLoaded>().unwrap();
-        for ((coord, chunk_data), entity) in self
-            .chunk_coords
+        // Split into coords/chunks and bundles while keeping everything in one pass
+        let mut coords_chunks = Vec::with_capacity(self.bundles.len());
+        let bundles: Vec<B> = self
+            .bundles
             .into_iter()
-            .zip(self.chunk_datas)
-            .zip(entities)
-        {
-            staged.0.insert(coord, (entity, chunk_data));
+            .map(|(coord, chunk, bundle)| {
+                coords_chunks.push((coord, chunk));
+                bundle
+            })
+            .collect();
+        // Batch spawn bundles
+        let entities: Vec<Entity> = world.spawn_batch(bundles).collect();
+        let mut staged = world.get_resource_mut::<StagedChunksLoaded>().unwrap();
+        // Pair entities with coords/chunks without separate zipping of pre-existing vecs
+        for ((coord, chunk), entity) in coords_chunks.into_iter().zip(entities) {
+            staged.0.insert(coord, (entity, chunk));
         }
     }
 }
@@ -160,14 +168,11 @@ pub fn catch_chunk_generation_request(
                     .collect::<Vec<_>>();
                 let mut with_collider = Vec::new();
                 let mut without_collider = Vec::new();
-                for (coord, chunk, mesh, transform, collider_opt) in results {
-                    match collider_opt {
-                        Some(collider) => {
-                            with_collider.push((coord, chunk, mesh, transform, collider));
-                        }
-                        None => {
-                            without_collider.push((coord, chunk, mesh, transform));
-                        }
+                for (coord, chunk, mesh_option, transform, collider_opt) in results {
+                    if let (Some(collider), Some(mesh)) = (collider_opt, mesh_option) {
+                        with_collider.push((coord, chunk, mesh, transform, collider));
+                    } else {
+                        without_collider.push((coord, chunk, transform));
                     }
                 }
                 (with_collider, without_collider)
@@ -203,27 +208,27 @@ pub fn catch_load_generation_request(
                         SDF_VALUES_PER_CHUNK_DIM,
                     );
                     let transform = Transform::from_translation(chunk_coord_to_world_pos(coord));
-                    let collider = if mesh.count_vertices() > 0 {
-                        Collider::from_bevy_mesh(
-                            &mesh,
-                            &ComputedColliderShape::TriMesh(TriMeshFlags::default()),
+                    let (collider, return_mesh) = if mesh.count_vertices() > 0 {
+                        (
+                            Collider::from_bevy_mesh(
+                                &mesh,
+                                &ComputedColliderShape::TriMesh(TriMeshFlags::default()),
+                            ),
+                            Some(mesh),
                         )
                     } else {
-                        None
+                        (None, None)
                     };
-                    (*coord, terrain_chunk, mesh, transform, collider)
+                    (*coord, terrain_chunk, return_mesh, transform, collider)
                 })
                 .collect::<Vec<_>>();
             let mut with_collider = Vec::new();
             let mut without_collider = Vec::new();
             for (coord, chunk, mesh, transform, collider_opt) in results {
-                match collider_opt {
-                    Some(collider) => {
-                        with_collider.push((coord, chunk, mesh, transform, collider));
-                    }
-                    None => {
-                        without_collider.push((coord, chunk, mesh, transform));
-                    }
+                if let (Some(collider), Some(mesh)) = (collider_opt, mesh) {
+                    with_collider.push((coord, chunk, mesh, transform, collider));
+                } else {
+                    without_collider.push((coord, chunk, transform));
                 }
             }
             (with_collider, without_collider)
@@ -238,7 +243,7 @@ pub fn generate_chunk_data(
 ) -> (
     (i16, i16, i16),
     TerrainChunk,
-    Mesh,
+    Option<Mesh>,
     Transform,
     Option<Collider>,
 ) {
@@ -249,15 +254,18 @@ pub fn generate_chunk_data(
         SDF_VALUES_PER_CHUNK_DIM,
     );
     let transform = Transform::from_translation(chunk_coord_to_world_pos(coord));
-    let collider = if mesh.count_vertices() > 0 {
-        Collider::from_bevy_mesh(
-            &mesh,
-            &ComputedColliderShape::TriMesh(TriMeshFlags::default()),
+    let (collider, return_mesh) = if mesh.count_vertices() > 0 {
+        (
+            Collider::from_bevy_mesh(
+                &mesh,
+                &ComputedColliderShape::TriMesh(TriMeshFlags::default()),
+            ),
+            Some(mesh),
         )
     } else {
-        None
+        (None, None)
     };
-    (*coord, terrain_chunk, mesh, transform, collider)
+    (*coord, terrain_chunk, return_mesh, transform, collider)
 }
 
 pub fn spawn_generated_chunks(
@@ -271,30 +279,20 @@ pub fn spawn_generated_chunks(
     my_tasks.generation_tasks.retain_mut(|task| {
         if task.is_finished() {
             let (bundle_data_with_collider, bundle_data_without_collider) = block_on(task);
-            let (
-                final_bundles_without_collider,
-                chunk_data_without_collider,
-                coords_without_collider,
-                final_bundles_with_collider,
-                chunk_data_with_collider,
-                coords_with_collider,
-            ) = spawn_chunks_from_source_task(
-                bundle_data_with_collider,
-                bundle_data_without_collider,
-                &player_transform.translation,
-                &mut chunk_coords_to_be_removed,
-                &mut meshes,
-                &material_handle.0,
-            );
+            let (final_bundles_without_collider, final_bundles_with_collider) =
+                spawn_chunks_from_source_task(
+                    bundle_data_with_collider,
+                    bundle_data_without_collider,
+                    &player_transform.translation,
+                    &mut chunk_coords_to_be_removed,
+                    &mut meshes,
+                    &material_handle.0,
+                );
             let chunk_spawner = ChunkSpawnCommand {
                 bundles: final_bundles_without_collider,
-                chunk_datas: chunk_data_without_collider,
-                chunk_coords: coords_without_collider,
             };
             let chunk_spawner_with_collider = ChunkSpawnCommand {
                 bundles: final_bundles_with_collider,
-                chunk_datas: chunk_data_with_collider,
-                chunk_coords: coords_with_collider,
             };
             commands.queue(chunk_spawner);
             commands.queue(chunk_spawner_with_collider);
@@ -318,30 +316,20 @@ pub fn finish_chunk_loading(
     my_tasks.loading_tasks.retain_mut(|task| {
         if task.is_finished() {
             let (bundle_data_with_collider, bundle_data_without_collider) = block_on(task);
-            let (
-                final_bundles_without_collider,
-                chunk_data_without_collider,
-                coords_without_collider,
-                final_bundles_with_collider,
-                chunk_data_with_collider,
-                coords_with_collider,
-            ) = spawn_chunks_from_source_task(
-                bundle_data_with_collider,
-                bundle_data_without_collider,
-                &player_transform.translation,
-                &mut chunk_coords_to_be_removed,
-                &mut meshes,
-                &material_handle.0,
-            );
+            let (final_bundles_without_collider, final_bundles_with_collider) =
+                spawn_chunks_from_source_task(
+                    bundle_data_with_collider,
+                    bundle_data_without_collider,
+                    &player_transform.translation,
+                    &mut chunk_coords_to_be_removed,
+                    &mut meshes,
+                    &material_handle.0,
+                );
             let chunk_spawner = ChunkLoadCommand {
                 bundles: final_bundles_without_collider,
-                chunk_datas: chunk_data_without_collider,
-                chunk_coords: coords_without_collider,
             };
             let chunk_spawner_with_collider = ChunkLoadCommand {
                 bundles: final_bundles_with_collider,
-                chunk_datas: chunk_data_with_collider,
-                chunk_coords: coords_with_collider,
             };
             commands.queue(chunk_spawner);
             commands.queue(chunk_spawner_with_collider);
@@ -355,88 +343,72 @@ pub fn finish_chunk_loading(
 }
 
 fn spawn_chunks_from_source_task(
-    mut bundle_data_with_collider: Vec<((i16, i16, i16), TerrainChunk, Mesh, Transform, Collider)>,
-    mut bundle_data_without_collider: Vec<((i16, i16, i16), TerrainChunk, Mesh, Transform)>,
+    bundle_data_with_collider: Vec<((i16, i16, i16), TerrainChunk, Mesh, Transform, Collider)>,
+    bundle_data_without_collider: Vec<((i16, i16, i16), TerrainChunk, Transform)>,
     player_translation: &Vec3,
     chunk_coords_to_be_removed: &mut Vec<(i16, i16, i16)>,
     meshes: &mut ResMut<Assets<Mesh>>,
     material_handle: &Handle<StandardMaterial>,
 ) -> (
+    Vec<((i16, i16, i16), TerrainChunk, (ChunkTag, Transform))>,
     Vec<(
-        Mesh3d,
-        ChunkTag,
-        Transform,
-        MeshMaterial3d<StandardMaterial>,
+        (i16, i16, i16),
+        TerrainChunk,
+        (
+            Mesh3d,
+            ChunkTag,
+            Transform,
+            MeshMaterial3d<StandardMaterial>,
+            Collider,
+        ),
     )>,
-    Vec<TerrainChunk>,
-    Vec<(i16, i16, i16)>,
-    Vec<(
-        Mesh3d,
-        ChunkTag,
-        Transform,
-        MeshMaterial3d<StandardMaterial>,
-        Collider,
-    )>,
-    Vec<TerrainChunk>,
-    Vec<(i16, i16, i16)>,
 ) {
     let start = std::time::Instant::now();
     let current_player_chunk = world_pos_to_chunk_coord(player_translation);
     let player_chunk_world_pos = chunk_coord_to_world_pos(&current_player_chunk);
+    //reserve space
+    chunk_coords_to_be_removed
+        .reserve(bundle_data_with_collider.len() + bundle_data_without_collider.len());
     chunk_coords_to_be_removed.extend(bundle_data_with_collider.iter().map(|(coord, ..)| *coord));
     chunk_coords_to_be_removed.extend(
         bundle_data_without_collider
             .iter()
             .map(|(coord, ..)| *coord),
     );
-    bundle_data_with_collider.retain(|(_chunk_coord, _chunkj, _mesh, transform, _collider)| {
-        transform
-            .translation
-            .distance_squared(player_chunk_world_pos)
-            <= CHUNK_CREATION_RADIUS_SQUARED
-    });
-    bundle_data_without_collider.retain(|(_chunk_coord, _chunk, _mesh, transform)| {
-        transform
-            .translation
-            .distance_squared(player_chunk_world_pos)
-            <= CHUNK_CREATION_RADIUS_SQUARED
-    });
-    let mut coords_with_collider = Vec::with_capacity(bundle_data_with_collider.len());
-    let mut chunk_data_with_collider = Vec::with_capacity(bundle_data_with_collider.len());
-    let mut coords_without_collider = Vec::with_capacity(bundle_data_without_collider.len());
-    let mut chunk_data_without_collider = Vec::with_capacity(bundle_data_without_collider.len());
-    let mut final_bundles_with_collider = Vec::with_capacity(bundle_data_with_collider.len());
     let material = MeshMaterial3d(material_handle.clone());
-    for (coords, chunk, mesh, transform, collider) in bundle_data_with_collider.into_iter() {
-        coords_with_collider.push(coords);
-        chunk_data_with_collider.push(chunk);
-        final_bundles_with_collider.push((
-            Mesh3d(meshes.add(mesh)),
-            ChunkTag,
-            transform,
-            material.clone(),
-            collider,
-        ));
-    }
-    let mut final_bundles_without_collider = Vec::with_capacity(bundle_data_without_collider.len());
-    for (coords, chunk, mesh, transform) in bundle_data_without_collider.into_iter() {
-        coords_without_collider.push(coords);
-        chunk_data_without_collider.push(chunk);
-        final_bundles_without_collider.push((
-            Mesh3d(meshes.add(mesh)),
-            ChunkTag,
-            transform,
-            material.clone(),
-        ));
-    }
+    let final_bundles_with_collider: Vec<_> = bundle_data_with_collider
+        .into_iter()
+        .filter_map(|(coord, chunk, mesh, transform, collider)| {
+            (transform
+                .translation
+                .distance_squared(player_chunk_world_pos)
+                <= CHUNK_CREATION_RADIUS_SQUARED)
+                .then(|| {
+                    (
+                        coord,
+                        chunk,
+                        (
+                            Mesh3d(meshes.add(mesh)),
+                            ChunkTag,
+                            transform,
+                            material.clone(),
+                            collider,
+                        ),
+                    )
+                })
+        })
+        .collect();
+    let final_bundles_without_collider: Vec<_> = bundle_data_without_collider
+        .into_iter()
+        .filter_map(|(coord, chunk, transform)| {
+            (transform
+                .translation
+                .distance_squared(player_chunk_world_pos)
+                <= CHUNK_CREATION_RADIUS_SQUARED)
+                .then(|| (coord, chunk, (ChunkTag, transform)))
+        })
+        .collect();
     let duration = start.elapsed();
     println!("finished loading chunks in {:?}", duration);
-    (
-        final_bundles_without_collider,
-        chunk_data_without_collider,
-        coords_without_collider,
-        final_bundles_with_collider,
-        chunk_data_with_collider,
-        coords_with_collider,
-    )
+    (final_bundles_without_collider, final_bundles_with_collider)
 }
