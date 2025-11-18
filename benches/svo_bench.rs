32e4103f90f00faa31dac8c1e5ac949b5ebd3b88
Saratii <friedchicken14821@gmail.com>
2025-11-17 23:08:02 -0600

    a debug line was apperently taking 99.6% of cpu cycles on octree construction

---
+++ benches/svo_bench.rs
@@ -0,0 +1,29 @@
+use std::{collections::HashMap, hint::black_box};
+
+use bevy::math::Vec3;
+use criterion::{Criterion, criterion_group, criterion_main};
+use marching_cubes::{
+    data_loader::driver::ChunksBeingLoaded, sparse_voxel_octree::ChunkSvo,
+    terrain::terrain::MAX_RADIUS,
+};
+
+fn benchmark_svo_first_traversal(c: &mut Criterion) {
+    c.bench_function("bench_create_new_svo", |b| {
+        b.iter(|| {
+            let mut svo = ChunkSvo::new();
+            svo.root.fill_missing_chunks_in_radius(
+                &Vec3::ZERO,
+                MAX_RADIUS,
+                &mut ChunksBeingLoaded {
+                    chunks: HashMap::new(),
+                    request_id: 1,
+                },
+                &mut Vec::new(),
+            );
+            black_box(svo);
+        })
+    });
+}
+
+criterion_group!(benches, benchmark_svo_first_traversal);
+criterion_main!(benches);
